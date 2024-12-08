916c5c892fcccdbdba446dc6c709fa473b3d0c18
yexca <yexcano@gmail.com>
2024-12-08 22:33:50 +0900

    v1.3 多语言支持

--- main.go
+++ main.go
@@ -17,30 +17,59 @@ type MyMainWindow struct {
 	longLayout      *walk.Composite
 	explainLayout   *walk.Composite
 	countDownLayout *walk.Composite
+
+	// mainLayout
+	mainTitleLabel  *walk.Label
+	mainButtonLeft  *walk.PushButton
+	mainButtonright *walk.PushButton
+
 	// longLayout
-	screenComboBox *walk.ComboBox
-	restComboBox   *walk.ComboBox
+	screenComboBox        *walk.ComboBox
+	restComboBox          *walk.ComboBox
+	longTitle             *walk.Label
+	LongSelectEvery       *walk.Label
+	LongSelectMinute1     *walk.Label
+	LongSelectMinute2     *walk.Label
+	LongSelectRest        *walk.Label
+	LongSelectButtonLeft  *walk.PushButton
+	LongSelectButtonRight *walk.PushButton
+
+	// language
+	languageSelectLayout *walk.Composite
+	languageComboBox     *walk.ComboBox
+	languageButton       *walk.PushButton
+
 	// 说明界面
-	explainTitle *walk.Label
-	explainText  *walk.TextLabel
+	explainTitle       *walk.Label
+	explainText        *walk.TextLabel
+	explainButtonLeft  *walk.PushButton
+	explainButtonRight *walk.PushButton
+
 	// 倒计时界面
 	countDownTitle      *walk.Label
 	countDownText       *walk.Label
 	countDownButton     *walk.PushButton
 	countDownBackButton *walk.PushButton
+
 	// 当前模式与持续次数
-	mode  int // 1 为短频，2 为高频
+	// mode  int // 1 为短频，2 为高频
 	count int
 	// 计时还是休息
 	state int
 	// 长时的选择
 	screenTime int
 	restTime   int
+	// 语言
+	language string
+	// 通知图标
+	notifyIcon string
 }
 
 func newMyMainWindow() *MyMainWindow {
 	return &MyMainWindow{
-		count: 0,
+		count:      0,
+		notifyIcon: "./icon/app.ico",
+		language:   "English",
 	}
 }
 
@@ -55,34 +84,88 @@ func main() {
 	// 长时的时间
 	screenSelect := []string{"30", "35", "40", "45"}
 	restSelect := []string{"5", "10"}
+	// language
+	languageSelect := []string{"English", "简体中文", "日本語"}
 
 	err = MainWindow{
 		AssignTo: &mw.MainWindow,
 		Title:    "yasumiProject",               // 窗口标题
-		MinSize:  Size{Width: 300, Height: 500}, // 最小尺寸
-		Size:     Size{Width: 300, Height: 500}, // 尺寸
+		MinSize:  Size{Width: 400, Height: 600}, // 最小尺寸
+		Size:     Size{Width: 400, Height: 600}, // 尺寸
+		MaxSize:  Size{Width: 450, Height: 650}, // 尺寸
 		Layout:   VBox{},                        // 窗口布局
 		//MenuItems: []MenuItem{},
 		Children: []Widget{
 			Composite{
 				AssignTo: &mw.mainLayout,
-				Visible:  true,
+				Visible:  false,
 				Layout:   VBox{},
 				Children: []Widget{
 					VSpacer{},
 					Label{
-						Text:      "请选择模式",
+						AssignTo:  &mw.mainTitleLabel,
+						Text:      "",
 						Alignment: AlignHCenterVCenter,
 						Font:      Font{PointSize: 14},
 					},
 					VSpacer{},
 					PushButton{
-						Text:      "短时间高频休息",
+						AssignTo:  &mw.mainButtonLeft,
+						Text:      "",
 						OnClicked: mw.shortLayoutToggle,
 					},
 					PushButton{
-						Text:      "长时间低频休息",
+						AssignTo:  &mw.mainButtonright,
+						Text:      "",
 						OnClicked: mw.LongSelectLayout,
+					},
+					VSpacer{},
+				},
+			},
+			Composite{
+				AssignTo: &mw.languageSelectLayout,
+				Visible:  true,
+				Layout:   VBox{},
+				Children: []Widget{
+					VSpacer{},
+					Label{
+						Text: "Please select language\n" + "请选择语言\n" + "言語を選んでください",
+						//MaxSize: Size{Height: 150, Width: 400},
+					},
+					//Label{
+					//	Text: "Please select language",
+					//},
+					//Label{
+					//	Text: "请选择语言",
+					//},
+					//Label{
+					//	Text: "言語を選んでください",
+					//},
+
+					ComboBox{
+						AssignTo:     &mw.languageComboBox,
+						Model:        languageSelect,
+						CurrentIndex: 1,
+						//MaxSize:      Size{Height: 100, Width: 200},
+						OnCurrentIndexChanged: func() {
+							mw.language = languageSelect[mw.languageComboBox.CurrentIndex()]
+							switch mw.language {
+							case "English":
+								mw.languageButton.SetText("next")
+							case "简体中文":
+								mw.languageButton.SetText("下一步")
+							case "日本語":
+								mw.languageButton.SetText("次へ")
+							}
+						},
+					},
+					VSpacer{},
+					PushButton{
+						AssignTo: &mw.languageButton,
+						Text:     "下一步",
+						OnClicked: func() {
+							mw.mainLayoutToggle()
+						},
 					},
 					VSpacer{},
 				},
@@ -107,7 +190,8 @@ func main() {
 				Children: []Widget{
 					VSpacer{},
 					Label{
-						Text:          "长时间低频休息",
+						AssignTo:      &mw.longTitle,
+						Text:          "",
 						TextAlignment: AlignCenter,
 						Font:          Font{PointSize: 20, Bold: true},
 					},
@@ -116,7 +200,8 @@ func main() {
 						Layout: HBox{},
 						Children: []Widget{
 							Label{
-								Text:      "学习",
+								AssignTo:  &mw.LongSelectEvery,
+								Text:      "",
 								Alignment: AlignHCenterVCenter,
 							},
 							ComboBox{
@@ -125,7 +210,8 @@ func main() {
 								CurrentIndex: 0,
 							},
 							Label{
-								Text:      "分钟",
+								AssignTo:  &mw.LongSelectMinute1,
+								Text:      "",
 								Alignment: AlignHCenterVCenter,
 							},
 						},
@@ -134,7 +220,8 @@ func main() {
 						Layout: HBox{},
 						Children: []Widget{
 							Label{
-								Text:      "休息",
+								AssignTo:  &mw.LongSelectRest,
+								Text:      "",
 								Alignment: AlignHCenterVCenter,
 							},
 							ComboBox{
@@ -143,7 +230,8 @@ func main() {
 								CurrentIndex: 0,
 							},
 							Label{
-								Text:      "分钟",
+								AssignTo:  &mw.LongSelectMinute2,
+								Text:      "",
 								Alignment: AlignHCenterVCenter,
 							},
 						},
@@ -153,11 +241,13 @@ func main() {
 						Layout: HBox{},
 						Children: []Widget{
 							PushButton{
-								Text:      "返回",
+								AssignTo:  &mw.LongSelectButtonLeft,
+								Text:      "",
 								OnClicked: mw.mainLayoutToggle,
 							},
 							PushButton{
-								Text: "开始",
+								AssignTo: &mw.LongSelectButtonRight,
+								Text:     "",
 								OnClicked: func() {
 									screenTime, _ := strconv.Atoi(screenSelect[mw.screenComboBox.CurrentIndex()])
 									restTime, _ := strconv.Atoi(restSelect[mw.restComboBox.CurrentIndex()])
@@ -170,7 +260,7 @@ func main() {
 					},
 				},
 			},
-			// 说明的界面
+			// (短时)说明的界面
 			Composite{
 				AssignTo: &mw.explainLayout,
 				Visible:  false,
@@ -192,20 +282,23 @@ func main() {
 						Layout: HBox{},
 						Children: []Widget{
 							PushButton{
-								Text:      "返回",
+								AssignTo:  &mw.explainButtonLeft,
+								Text:      "",
 								OnClicked: mw.mainLayoutToggle,
 							},
 							PushButton{
-								Text: "开始",
+								AssignTo: &mw.explainButtonRight,
+								Text:     "",
 								OnClicked: func() {
-									if mw.mode == 1 {
-										mw.shortScreenCountDownLayout()
-									} else {
-										err := beeep.Notify("a", "b", "./icon/app.ico")
-										if err != nil {
-											return
-										}
-									}
+									mw.shortScreenCountDownLayout()
+									//if mw.mode == 1 {
+									//	mw.shortScreenCountDownLayout()
+									//} else {
+									//	err := beeep.Notify("a", "b", "./icon/app.ico")
+									//	if err != nil {
+									//		return
+									//	}
+									//}
 								},
 							},
 						},
@@ -235,14 +328,14 @@ func main() {
 					VSpacer{},
 					PushButton{
 						AssignTo: &mw.countDownButton,
-						Text:     "计时中···",
+						Text:     "",
 						Enabled:  false,
 						OnClicked: func() {
 						},
 					},
 					PushButton{
 						AssignTo:  &mw.countDownBackButton,
-						Text:      "主界面",
+						Text:      "",
 						Enabled:   true,
 						OnClicked: mw.mainLayoutToggle,
 					},
@@ -254,9 +347,11 @@ func main() {
 		return
 	}
 
-	mw.mainLayoutToggle()
+	mw.clearLayout()
+	mw.SetSize(walk.Size{Width: 400, Height: 600})
+	mw.languageSelectLayout.SetVisible(true)
 
-	icon, err := walk.NewIconFromFile("./icon/app.ico")
+	icon, err := walk.NewIconFromFile(mw.notifyIcon)
 	if err != nil {
 		return
 	}
@@ -264,58 +359,12 @@ func main() {
 
 	app.Run()
 
-	//{
-	//	Label{Text: "请选择模式", Alignment: AlignHCenterVCenter}.Create(NewBuilder(dynamicComposite))
-	//	PushButton{Text: "短时间高频休息"}.Create(NewBuilder(dynamicComposite))
-	//
-	//	//dynamicComposite.Layout().Spacing()
-	//}
-
-	//MainWindow{
-	//	AssignTo: &mainWindow,
-	//	Title:    "Hi walk",                     // 窗口标题
-	//	MinSize:  Size{Width: 400, Height: 500}, // 最小尺寸
-	//	Size:     Size{Width: 400, Height: 500}, // 尺寸
-	//	Layout:   VBox{},                        // 窗口布局
-	//	Children: []Widget{
-	//		PushButton{
-	//			Text: "打开新窗口",
-	//			OnClicked: func() {
-	//				// 调用函数打开新窗口
-	//				openNewWindow(mainWindow)
-	//			},
-	//		},
-	//	},
-	//}.Create()
-
-	//app.Run()
 }
-
-//	func openNewWindow(owner walk.Form) {
-//		var newWindow *walk.MainWindow
-//
-//		MainWindow{
-//			AssignTo: &newWindow,
-//			Title:    "新窗口",
-//			MinSize:  Size{Width: 300, Height: 200},
-//			Layout:   VBox{},
-//			Children: []Widget{
-//				Label{
-//					Text: "这是一个新窗口",
-//				},
-//				PushButton{
-//					Text: "关闭窗口",
-//					OnClicked: func() {
-//						newWindow.Close()
-//					},
-//				},
-//			},
-//		}.Create()
-//	}
 
 // 清除所有控件显示
 func (mw *MyMainWindow) clearLayout() {
 	mw.mainLayout.SetVisible(false)
+	mw.languageSelectLayout.SetVisible(false)
 	//mw.shortLayout.SetVisible(false)
 	mw.longLayout.SetVisible(false)
 	mw.explainLayout.SetVisible(false)
@@ -325,6 +374,9 @@ func (mw *MyMainWindow) clearLayout() {
 // 主界面
 func (mw *MyMainWindow) mainLayoutToggle() {
 	mw.clearLayout()
+	mw.mainTitleLabel.SetText(GetText(mw.language, "modeSelect"))
+	mw.mainButtonLeft.SetText(GetText(mw.language, "shortMode"))
+	mw.mainButtonright.SetText(GetText(mw.language, "longMode"))
 	mw.mainLayout.SetVisible(true)
 }
 
@@ -333,12 +385,13 @@ func (mw *MyMainWindow) shortLayoutToggle() {
 	mw.clearLayout()
 	//mw.shortLayout.SetVisible(true)
 
-	mw.explainTitle.SetText("短时间高频休息")
-	mw.explainText.SetText("此模式基于 20-20-20 规则：每使用屏幕 20 分钟，看向距离 20 英尺（约 6 米） 的物体 20 秒，可以有效缓解眼睛疲劳。\n\n" +
-		"但是考虑到点击造成的时间延迟，本程序采用每 20 分钟休息 1 分钟的模式，同时每三个 20 分钟将进行 5 分钟的休息")
+	mw.explainTitle.SetText(GetText(mw.language, "shortMode"))
+	mw.explainText.SetText(GetText(mw.language, "shortExplainText"))
+	mw.explainButtonLeft.SetText(GetText(mw.language, "back"))
+	mw.explainButtonRight.SetText(GetText(mw.language, "next"))
 
 	// 切换模式
-	mw.mode = 1
+	//mw.mode = 1
 
 	mw.explainLayout.SetVisible(true)
 }
@@ -347,16 +400,18 @@ func (mw *MyMainWindow) shortLayoutToggle() {
 func (mw *MyMainWindow) shortScreenCountDownLayout() {
 	mw.clearLayout()
 
-	mw.countDownTitle.SetText("短时间高频休息")
-	mw.countDownText.SetText("倒计时 20 分钟")
+	mw.countDownTitle.SetText(GetText(mw.language, "shortMode"))
+	mw.countDownText.SetText(GetText(mw.language, "countDown") + " 20 " + GetText(mw.language, "minute"))
 
 	// 计时界面按钮修改
-	mw.countDownButton.SetText("开始学习")
+	mw.countDownButton.SetText(GetText(mw.language, "startStudyButton"))
 	mw.countDownButton.Clicked().Detach(0)
 	mw.countDownButton.Clicked().Attach(func() {
 		go countDown(1200, mw)
 	})
 	mw.countDownButton.SetEnabled(true)
+
+	mw.countDownBackButton.SetText(GetText(mw.language, "home"))
 
 	mw.count += 1
 
@@ -371,17 +426,17 @@ func (mw *MyMainWindow) shortScreenCountDownLayout() {
 func (mw *MyMainWindow) shortRestCountDownLayout() {
 	mw.clearLayout()
 
-	err := mw.countDownTitle.SetText("短时间高频休息")
+	err := mw.countDownTitle.SetText(GetText(mw.language, "shortMode"))
 	if err != nil {
 		return
 	}
-	err = mw.countDownText.SetText("时间到，休息啦")
+	err = mw.countDownText.SetText(GetText(mw.language, "restLabel"))
 	if err != nil {
 		return
 	}
 
 	// 计时界面按钮修改
-	err = mw.countDownButton.SetText("开始休息")
+	err = mw.countDownButton.SetText(GetText(mw.language, "startRestButton"))
 	if err != nil {
 		return
 	}
@@ -406,6 +461,13 @@ func (mw *MyMainWindow) shortRestCountDownLayout() {
 // 长时选择界面
 func (mw *MyMainWindow) LongSelectLayout() {
 	mw.clearLayout()
+	mw.longTitle.SetText(GetText(mw.language, "longMode"))
+	mw.LongSelectEvery.SetText(GetText(mw.language, "every"))
+	mw.LongSelectMinute1.SetText(GetText(mw.language, "minute"))
+	mw.LongSelectMinute2.SetText(GetText(mw.language, "minute"))
+	mw.LongSelectRest.SetText(GetText(mw.language, "rest"))
+	mw.LongSelectButtonLeft.SetText(GetText(mw.language, "back"))
+	mw.LongSelectButtonRight.SetText(GetText(mw.language, "next"))
 	mw.longLayout.SetVisible(true)
 	//beeep.Notify("Error", "还没做捏", "./icon/app.ico")
 }
@@ -414,16 +476,18 @@ func (mw *MyMainWindow) LongSelectLayout() {
 func (mw *MyMainWindow) longScreenCountDownLayout() {
 	mw.clearLayout()
 
-	mw.countDownTitle.SetText("长时间低频休息")
-	mw.countDownText.SetText(fmt.Sprintf("倒计时 %d 分钟", mw.screenTime))
+	mw.countDownTitle.SetText(GetText(mw.language, "longMode"))
+	mw.countDownText.SetText(GetText(mw.language, "countDown") + fmt.Sprintf(" %d ", mw.screenTime) + GetText(mw.language, "minute"))
 
 	// 计时界面按钮修改
-	mw.countDownButton.SetText("开始学习")
+	mw.countDownButton.SetText(GetText(mw.language, "startStudyButton"))
 	mw.countDownButton.Clicked().Detach(0)
 	mw.countDownButton.Clicked().Attach(func() {
 		go countDown(mw.screenTime*60, mw)
 	})
 	mw.countDownButton.SetEnabled(true)
+
+	mw.countDownBackButton.SetText(GetText(mw.language, "home"))
 
 	// 看屏幕状态
 	mw.state = 1
@@ -436,17 +500,17 @@ func (mw *MyMainWindow) longScreenCountDownLayout() {
 func (mw *MyMainWindow) longRestCountDownLayout() {
 	mw.clearLayout()
 
-	err := mw.countDownTitle.SetText("长时间低频休息")
+	err := mw.countDownTitle.SetText(GetText(mw.language, "longMode"))
 	if err != nil {
 		return
 	}
-	err = mw.countDownText.SetText("时间到，休息啦")
+	err = mw.countDownText.SetText(GetText(mw.language, "restLabel"))
 	if err != nil {
 		return
 	}
 
 	// 计时界面按钮修改
-	err = mw.countDownButton.SetText("开始休息")
+	err = mw.countDownButton.SetText(GetText(mw.language, "startRestButton"))
 	if err != nil {
 		return
 	}
@@ -470,12 +534,12 @@ func countDown(seconds int, mw *MyMainWindow) {
 
 	// 根据状态调用通知
 	if mw.state == 1 {
-		beeep.Notify("学习时间", "开始计时，一起努力加油吧", "./icon/app.ico")
+		beeep.Notify(GetText(mw.language, "studyStartNotifyTitle"), GetText(mw.language, "studyStartNotifyContent"), mw.notifyIcon)
 	} else if mw.state == 2 {
-		beeep.Notify("休息时间", "开始休息，记得向 6 米外的地方至少看 20 秒哦", "./icon/app.ico")
+		beeep.Notify(GetText(mw.language, "restStartNotifyTitle"), GetText(mw.language, "restStartNotifyContent"), mw.notifyIcon)
 	}
 
-	mw.countDownButton.SetText("计时中···")
+	mw.countDownButton.SetText(GetText(mw.language, "Timing"))
 	mw.countDownButton.SetEnabled(false)
 
 	for i := seconds; i >= 0; i-- {
@@ -484,15 +548,15 @@ func countDown(seconds int, mw *MyMainWindow) {
 
 		time.Sleep(1 * time.Second)
 
-		mw.countDownText.SetText(fmt.Sprintf("剩余时间：%2d:%2d", m, s))
+		mw.countDownText.SetText(GetText(mw.language, "remain") + fmt.Sprintf("：%2d:%2d", m, s))
 	}
 
 	// 根据状态调用界面
 	if mw.state == 1 {
-		beeep.Notify("休息时间", "休息时间到了，快点开始休息吧", "./icon/app.ico")
+		beeep.Notify(GetText(mw.language, "restStartNotifyTitle"), GetText(mw.language, "studyEndNotifyContent"), mw.notifyIcon)
 		mw.shortRestCountDownLayout()
 	} else if mw.state == 2 {
-		beeep.Notify("学习时间", "休息结束，继续一起努力学习吧", "./icon/app.ico")
+		beeep.Notify(GetText(mw.language, "studyStartNotifyTitle"), GetText(mw.language, "restEndNotifyContent"), mw.notifyIcon)
 		mw.longScreenCountDownLayout()
 	}
 
